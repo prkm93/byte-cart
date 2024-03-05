b92fa44a69685f0c465b32b1aeba8527c2c31b85
Pradeep Maurya <aradshub93@gmail.com>
2024-03-05 10:39:42 +0530

    updated styles for product card

--- src/pages/Product/ProductCard/ProductCard.js
+++ src/pages/Product/ProductCard/ProductCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoIosHeartEmpty } from "react-icons/io";
 import styles from "./ProductCard.module.css";
 
 const ProductCard = (product) => {
@@ -16,18 +17,20 @@ const ProductCard = (product) => {
     discountPercentage,
   } = product.product;
   console.log("product", product.product);
-  //   const originalPrice = Math.floor(price + (price * discountPercentage) / 100);
+  const originalPrice = Math.floor(price + (price * discountPercentage) / 100);
 
   return (
     <div className={styles.product_card}>
       <div>
         <img className={styles.product_img} src={thumbnail} alt={title} />
       </div>
-      <div>{title}</div>
-      <div>
-        <div>{price}</div>
-        {/* <div>{originalPrice}</div> */}
+      <IoIosHeartEmpty className={styles.wishlist_icon} />
+      <div className={styles.card_title}>{title}</div>
+      <div className={styles.card_price_details}>
+        <div>₹ {price}</div>
+        <div>₹ {originalPrice}</div>
       </div>
+      <button className={styles.cart_btn}>Add to cart</button>
     </div>
   );
 };
