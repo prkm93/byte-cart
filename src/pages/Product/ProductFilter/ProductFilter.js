b68c9d0323e0ac7e4ab08107f89532db393dcecb
Pradeep Maurya <aradshub93@gmail.com>
2024-06-23 12:03:08 +0530

    added interaction to filter stored in state

--- src/pages/Product/ProductFilter/ProductFilter.js
+++ src/pages/Product/ProductFilter/ProductFilter.js
@@ -1,31 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useProducts } from "../../../context/ProductContext";
 import { discountedPrice, capitalise1stChar } from "../../../utils/utils";
+import { filterTypes } from "../../../utils/constant";
 import styles from "./ProductFilter.module.css";
 
 const ProductFilter = () => {
   const {
-    productState: { productList, categoryList },
+    productState: {
+      productList,
+      categoryList,
+      categoryInput,
+      priceRangeInput,
+      ratingInput,
+      sortByPrice,
+    },
+    productDispatch,
   } = useProducts();
+  const {
+    FILTER_BY_PRICE,
+    FILTER_BY_CATEGORY,
+    FILTER_BY_RATING,
+    SORT,
+    CLEAR_FILTER,
+  } = filterTypes;
 
   const prices = productList
     .map((product) =>
       discountedPrice(product.price, product.discountPercentage)
     )
     .sort((a, b) => b - a);
-  const maxPrice = prices[0];
+  const maxPrice = Math.ceil(prices[0] / 10) * 10;
   const midPrice = Math.floor(maxPrice / 2);
 
   const categories = categoryList.map(({ categoryName }) => categoryName);
   const Ratings = [4, 3, 2, 1];
-
-  const [price, setPrice] = useState(midPrice);
-
+  console.log(useProducts().productState);
   return (
     <div className={styles.filter_section}>
       <div className={styles.filter_header}>
         <div className={styles.filter_label}>Filters</div>
-        <div className={styles.clear_text}>Clear</div>
+        <div
+          className={styles.clear_text}
+          onClick={() =>
+            productDispatch({
+              type: CLEAR_FILTER,
+              payload: {
+                productList: productList,
+                categoryList: categoryList,
+              },
+            })
+          }>
+          Clear
+        </div>
       </div>
       <div className={styles.price_filter}>
         <label htmlFor="price_range" className={styles.price_label}>
@@ -43,10 +69,16 @@ const ProductFilter = () => {
           id="price_range"
           min={0}
           max={maxPrice}
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
+          step={50}
+          value={priceRangeInput}
+          onChange={(e) =>
+            productDispatch({
+              type: FILTER_BY_PRICE,
+              payload: e.target.value,
+            })
+          }
         />
-        <label className={styles.current_price}>{price}</label>
+        <label className={styles.current_price}>{priceRangeInput}</label>
       </div>
       <div className={styles.category_filter}>
         <label htmlFor="categories" className={styles.category_label}>
@@ -57,8 +89,15 @@ const ProductFilter = () => {
             <div className={styles.checkbox} key={`${item}-checkbox`}>
               <input
                 type="checkbox"
-                name={`${item}-checkbox`}
+                name="category-checkbox"
                 id={`${item}-checkbox`}
+                checked={categoryInput.includes(item)}
+                onChange={() =>
+                  productDispatch({
+                    type: FILTER_BY_CATEGORY,
+                    payload: item,
+                  })
+                }
               />
               <label htmlFor={`${item}-checkbox`}>
                 {capitalise1stChar(item)}
@@ -74,7 +113,18 @@ const ProductFilter = () => {
         {Ratings.map((rating) => {
           return (
             <div className={styles.checkbox} key={`${rating}-star`}>
-              <input type="radio" name="rating" id={`${rating}-star`} />
+              <input
+                type="radio"
+                name="rating"
+                id={`${rating}-star`}
+                checked={Number(ratingInput) === Number(rating)}
+                onChange={() =>
+                  productDispatch({
+                    type: FILTER_BY_RATING,
+                    payload: rating,
+                  })
+                }
+              />
               <label htmlFor={`${rating}-star`}>{rating} star & above</label>
             </div>
           );
@@ -85,11 +135,35 @@ const ProductFilter = () => {
           Sort by Price
         </label>
         <div className={styles.checkbox}>
-          <input type="radio" name="sort" id="sortHighToLow" />
+          <input
+            type="radio"
+            name="sort"
+            id="sortHighToLow"
+            value="sortHighToLow"
+            checked={sortByPrice === "sortHighToLow"}
+            onChange={(e) =>
+              productDispatch({
+                type: SORT,
+                payload: e.target.value,
+              })
+            }
+          />
           <label htmlFor="sortHighToLow">Price - High to low</label>
         </div>
         <div className={styles.checkbox}>
-          <input type="radio" name="sort" id="sortLowToHigh" />
+          <input
+            type="radio"
+            name="sort"
+            id="sortLowToHigh"
+            value="sortLowToHigh"
+            checked={sortByPrice === "sortLowToHigh"}
+            onChange={(e) =>
+              productDispatch({
+                type: SORT,
+                payload: e.target.value,
+              })
+            }
+          />
           <label htmlFor="sortLowToHigh">Price - Low to high</label>
         </div>
       </div>
--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -2,7 +2,14 @@ import { productActionTypes, filterTypes } from "../utils/constant";
 
 const { FETCH_CATEGORIES, FETCH_PRODUCTS, IS_LOADING, GET_PRODUCT_DETAILS } =
   productActionTypes;
-const { SEARCH_PRODUCT } = filterTypes;
+const {
+  SEARCH_PRODUCT,
+  FILTER_BY_CATEGORY,
+  FILTER_BY_PRICE,
+  SORT,
+  FILTER_BY_RATING,
+  CLEAR_FILTER,
+} = filterTypes;
 
 export const initialProductState = {
   productList: [],
@@ -10,8 +17,8 @@ export const initialProductState = {
   productDetail: {},
   isLoadingItems: false,
   searchProduct: "",
-  priceRangeInput: "",
-  categoryInput: "",
+  priceRangeInput: 0,
+  categoryInput: [],
   ratingInput: "",
   sortByPrice: "",
 };
@@ -43,6 +50,41 @@ export const productReducer = (state = initialProductState, action) => {
       return {
         ...state,
         searchProduct: payload,
+      };
+    case FILTER_BY_PRICE:
+      return {
+        ...state,
+        priceRangeInput: payload,
+      };
+    case FILTER_BY_CATEGORY:
+      return {
+        ...state,
+        categoryInput: state.categoryInput.includes(payload.toLowerCase())
+          ? state.categoryInput.filter(
+              (item) => item.toLowerCase() !== payload.toLowerCase()
+            )
+          : [...state.categoryInput, payload.toLowerCase()],
+      };
+    case FILTER_BY_RATING:
+      return {
+        ...state,
+        ratingInput: payload,
+      };
+    case SORT:
+      return {
+        ...state,
+        sortByPrice: payload,
+      };
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        productList: payload.productList,
+        categoryList: payload.categoryList,
+        searchProduct: "",
+        priceRangeInput: 0,
+        categoryInput: [],
+        ratingInput: "",
+        sortByPrice: "",
       };
     default:
       return state;
