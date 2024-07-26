a27cafbc4ba94714b65b1bd6b8a9e7d416ec49ae
Pradeep Maurya <aradshub93@gmail.com>
2024-07-26 12:23:28 +0530

    refactor

--- src/components/AddressForm/AddressForm.jsx
+++ src/components/AddressForm/AddressForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Address.module.css";
+// import styles from "./Address.module.css";
 
 const AddressForm = () => {
   return <div>AddressForm</div>;
